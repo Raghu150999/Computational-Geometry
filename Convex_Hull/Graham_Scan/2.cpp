ef015990a66bb855617175c365f30a64e66d6263
Raghu <raghu15sep99@gmail.com>
2020-01-30 23:16:31 +0530

    Added Graham Scan (using angle sort)

--- Convex_Hull/Graham_Scan/2.cpp
+++ Convex_Hull/Graham_Scan/2.cpp
@@ -2,23 +2,28 @@
 
 using namespace std;
 
+#define INF 1e9
+
+/*
+*   This implementation performs Graham's Scan by sorting vertices
+*   by angle, with respect to pivot. Pivot is choosen to be the 
+*   left-most bottom-most point.
+*/
+
 struct Point
 {
     int x, y;
-
     Point(int a, int b)
     {
         x = a;
         y = b;
     }
-
     bool operator<(const Point &p)
     {
         if (x != p.x)
             return x < p.x;
         return y < p.y;
     }
-
     int cross(Point &p)
     {
         return (x * p.y - p.x * y);
@@ -39,12 +44,25 @@ struct Point
     }
 };
 
+Point pivot(INF, INF);
+
+bool compare(Point &p1, Point &p2)
+{
+    int o = p1.orient(pivot, p1, p2);
+    if(o == 1)
+        return 1;
+    else if(o == -1)
+        return 0;
+    else
+        return p1 < p2;
+}
+
 // Function to insert new point into the hull
 void insert(Point &p, stack<Point> &order)
 {
     Point top = order.top();
     order.pop();
-    while (order.size() >= 1 && top.orient(order.top(), top, p) >= 0)
+    while (order.size() >= 1 && top.orient(order.top(), top, p) <= 0)
     {
         top = order.top();
         order.pop();
@@ -53,12 +71,12 @@ void insert(Point &p, stack<Point> &order)
     order.push(p);
 }
 
-/* Following implementation is robust to:
-*   1. Repetition of points
-*   2. Collinear points
+/*  Following implementation is robust to:
+*       1. Repetition of points
+*       2. Collinear points
 */
 
-int32_t main()
+int main()
 {
     // Number of points in the plane
     int n;
@@ -68,9 +86,11 @@ int32_t main()
     {
         int x, y;
         cin >> x >> y;
-        vp.push_back(Point(x, y));
+        Point p(x, y);
+        vp.push_back(p);
+        if(p < pivot)
+            pivot = p;
     }
-
     // Handle case when n < 3
     if (n < 3)
     {
@@ -78,29 +98,33 @@ int32_t main()
         return 0;
     }
 
-    // Sort vertices by x-coordinate then by y-coordinate
-    sort(vp.begin(), vp.end());
+    // Sort vertices by angle from pivot
+    sort(vp.begin(), vp.end(), compare);
 
     stack<Point> order;
+    
     order.push(vp[0]);
     order.push(vp[1]);
-
-    // Construct Upper Hull
-    for (int i = 2; i < n; i++)
-        insert(vp[i], order);
-
-    order.push(vp[n - 2]);
-    // Construct the Lower Hull
-    for (int i = n - 3; i >= 0; i--)
+    
+    // Iterate over all points and insert into Convex Hull
+    for(int i = 2; i < n; i++)
         insert(vp[i], order);
 
     // Convex Hull in counter clockwise order
     cout << "Convex Hull (counter clockwise order): " << endl;
+    stack<Point> invert_order;
     while (!order.empty())
     {
         Point top = order.top();
-        cout << top.x << " " << top.y << endl;
+        invert_order.push(top);
         order.pop();
+    }
+
+    while(!invert_order.empty())
+    {
+        Point top = invert_order.top();
+        cout << top.x << " " << top.y << endl;
+        invert_order.pop();
     }
     return 0;
 }
